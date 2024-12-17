f750f1ae2bbe69962f4fdf65c4b5c48ad9d3f74d
Junior Facin <jufacin@hotmail.com>
2024-12-17 16:18:26 -0300

    initial Commit: Fundamentos do C#

---
+++ ContentContext/Career.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+
+namespace Balta.ContentContext
+{
+    public class Career : Content
+    {
+        public Career(string title, string url)
+            : base(title, url)
+        {
+            Items = new List<CareerItem>();
+        }
+        public IList<CareerItem> Items { get; set; }
+
+        // Expression Body
+        public int TotalCourses => Items.Count;
+    }
+
+}
---
+++ ContentContext/Content.cs
@@ -0,0 +1,12 @@
+public abstract class Content
+{
+    public Content(string title, string url)
+    {
+        Id = Guid.NewGuid(); // SPOF
+        Title = title;
+        Url = url;
+    }
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Url { get; set; }
+}
